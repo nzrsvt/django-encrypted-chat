646cb63dc008489239478270d0c4f0f0f400f9a0
nzrsvt <nsavitsky89@gmail.com>
2024-02-09 11:08:13 +0200

    Prepare the transition to private chats

--- chatapp/routing.py
+++ chatapp/routing.py
@@ -1,6 +1,7 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/public_room/", consumers.ChatConsumer.as_asgi()),
+    path('ws/chat/private/<str:user_id>/', consumers.PrivateChatConsumer.as_asgi()),
 ]
