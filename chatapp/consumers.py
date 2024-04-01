be93da86b2a4ad2383c6effa9e441adb09ea7ba1
nzrsvt <nsavitsky89@gmail.com>
2024-04-01 12:24:16 +0300

    Redesign the message processing mechanism: completely change the deletion of messages, other improvements

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -8,10 +8,10 @@ from channels.db import database_sync_to_async
 class PrivateChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.chat_id = self.scope['url_route']['kwargs']['chat_id']
-        self.chat_group_name = f"private_chat_{self.chat_id}"
+        self.chat_name = f"private_chat_{self.chat_id}"
 
         await self.channel_layer.group_add(
-            self.chat_group_name,
+            self.chat_name,
             self.channel_name
         )
 
@@ -19,12 +19,55 @@ class PrivateChatConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
-            self.chat_group_name,
+            self.chat_name,
             self.channel_name
         )
 
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message_text = data.get('message')
+        delete_message_id = data.get('delete_message')
+
+        if message_text:
+            message = await self.save_message(message_text)
+
+            await self.channel_layer.group_send(
+                self.chat_name,
+                {
+                    'type': 'chat_message',
+                    'message': message_text,
+                    'username': self.scope['user'].username,
+                    'message_id': message.id
+                }
+            )
+        elif delete_message_id:
+            try:
+                message = await database_sync_to_async(Message.objects.get)(id=delete_message_id)
+                await database_sync_to_async(message.delete)()
+                await self.channel_layer.group_send(
+                self.chat_name,
+                {
+                    'type': 'delete_message',
+                    'message_id': delete_message_id
+                }
+            )
+            except Message.DoesNotExist:
+                pass
+
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        message_id = event['message_id']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+            'message_id': message_id
+        }))
+
     @database_sync_to_async
-    def create_message(self, message_text):
+    def save_message(self, message_text):
         message = Message.objects.create(
             text=message_text,
             content_type=ContentType.objects.get_for_model(PrivateChat),
@@ -33,53 +76,8 @@ class PrivateChatConsumer(AsyncWebsocketConsumer):
         )
         return message
 
-    async def receive(self, text_data):
-        data = json.loads(text_data)
-        message_text = data.get('message')
-        delete_message_id = data.get('delete_message')
-
-        if message_text:
-            # Save the message to the database asynchronously
-            message = await self.create_message(message_text)
-
-            await self.channel_layer.group_send(
-                self.chat_group_name,
-                {
-                    'type': 'chat.message',
-                    'message': message_text,
-                    'username': self.scope['user'].username,
-                    'message_id': message.id  # Pass the message ID
-                }
-            )
-        elif delete_message_id:
-            # Delete the message
-            await self.delete_message(delete_message_id)
-
-    async def delete_message(self, message_id):
-        message = await database_sync_to_async(Message.objects.get)(id=message_id)
-        await database_sync_to_async(message.delete)()
-        await self.channel_layer.group_send(
-            self.chat_group_name,
-            {
-                'type': 'chat.message.deleted',
-                'message_id': message_id,
-            }
-        )
-
-    async def chat_message(self, event):
-        message = event['message']
-        username = event['username']
-        message_id = event['message_id']  # Retrieve the message ID
-
-        await self.send(text_data=json.dumps({
-            'message': message,
-            'username': username,
-            'message_id': message_id  # Pass the message ID
-        }))
-
-    async def chat_message_deleted(self, event):
+    async def delete_message(self, event):
         message_id = event['message_id']
-
         await self.send(text_data=json.dumps({
             'delete_message': message_id
         }))
--- encryptedchat/asgi.py
+++ encryptedchat/asgi.py
@@ -4,7 +4,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chatapp.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'encryptedchat.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
